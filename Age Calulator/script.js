c2c08f8f545106acb2651adcc08c3f3f980b119c
Meharsh Chandure <meharsh7804@gmail.com>
2024-05-23 18:21:58 +0530

    Day 1 Completed
    On Day 1 , I have created Age Calculator!

---
+++ Age Calulator/script.js
@@ -0,0 +1,20 @@
+let currDate = document.getElementById("currDate");
+let dateOfBirth = document.querySelector("#DOB");
+const CalcAge = document.getElementById("CalcAge");
+const displayAge = document.getElementById("displayAge");
+const Age = document.getElementById("age");
+var today = new Date();
+currDate.innerText = `Today's Date is : ${today.toLocaleDateString("en-US")}`;
+
+CalcAge.addEventListener("click", () => {
+  var birthDate = new Date(dateOfBirth.value);
+  var age = today.getFullYear() - birthDate.getFullYear();
+  var m = today.getMonth() - birthDate.getMonth();
+  if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+    age = age - 1;
+  }
+  displayAge.classList.add('animate');
+  Age.innerText = `You are ${age} years old.`;
+});
+
+
