c040328551fdd93c9f634c2f453a97348885da01
Meharsh Chandure <meharsh7804@gmail.com>
2024-05-29 16:43:29 +0530

    Day 7 Completed

---
+++ Cat Generator/script.js
@@ -0,0 +1,7 @@
+function generateCat() {
+  var image = document.createElement("img");
+  var div = document.getElementById("flex-cat-gen");
+  image.src =
+    "https://static.vecteezy.com/system/resources/thumbnails/022/963/918/small_2x/ai-generative-cute-cat-isolated-on-solid-background-photo.jpg";
+  div.appendChild(image);
+}
