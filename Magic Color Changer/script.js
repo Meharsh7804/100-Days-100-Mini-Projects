57053bcd1274f7bf0110a716048238658f52b525
Meharsh Chandure <meharsh7804@gmail.com>
2024-06-26 09:36:56 +0530

    Day 35 Completed

---
+++ Magic Color Changer/script.js
@@ -0,0 +1,13 @@
+document.getElementById("colorChanger").addEventListener("click", function () { 
+    const randomColor = getRandomColor();
+    document.body.style.backgroundColor = randomColor;
+});
+
+function getRandomColor(){
+    const letters = "0123456789ABCDEF";
+    let color = "#";
+    for (let i = 0; i < 6; i++){
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
